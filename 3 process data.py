298b90975ff73dbc38820d61684fec9fe090483e
dimitri-shvorob <dimitri.shvorob@gmail.com>
2025-07-20 07:08:20 +0100

    bug fixes

--- 3 process data.py
+++ 3 process data.py
@@ -17,7 +17,7 @@ for key, value in tariffs.items():
         effective_to=pl.col("effective_to").str.to_date(),
     )
     dts.append(dt)
-dt = pl.concat(dts, how="diagonal").sort("effective_from")
+dt = pl.concat(dts, how="diagonal").sort("type", "effective_from")
 
 # solis daily generation
 ds = pl.read_csv(PATH / "data solis.csv", try_parse_dates=True).drop_nulls(
@@ -29,18 +29,27 @@ df1 = (
     pl.read_parquet(PATH / "data octopus" / "*.parquet")
     .with_columns(date=pl.col("start_time").dt.date())
     .drop("start_time")
-    .group_by("date", "type")
+    .group_by("type", "date")
     .sum()
 )
 
 # date/type spine
+max_date_octopus = df1["date"].max()
+max_date_solis = ds["date"].max()
+print(f"max date octopus: {max_date_octopus}")
+print(f"max date solis: {max_date_solis}")
+max_date = max_date_solis if max_date_solis < max_date_octopus else max_date_octopus
+
 dg1 = pl.DataFrame(
-    data=pl.date_range(start=df1["date"].min(), end=df1["date"].max(), eager=True),
+    data=pl.date_range(start=df1["date"].min(), end=max_date, eager=True),
     schema=["date"],
 )
 dg2 = pl.DataFrame(data=["electric_import", "electric_export", "gas"], schema=["type"])
-df2 = dg1.join(dg2, how="cross").join(df1, on=["date", "type"], how="left")
-
+df2 = (dg1
+      .join(dg2, how="cross")\
+      .join(df1, on=["date", "type"], how="left")
+      .sort("type", "date")
+)
 # link up date spine, octopus usage and tariffs
 # fill electric export with zeroes
 df3 = (
@@ -56,14 +65,13 @@ df3 = (
         .otherwise(pl.col.value_kwh)
     )
     .with_columns(
-        value_gbp=(pl.col.standing_charge + pl.col.value_kwh * pl.col.charge_per_kwh)
-        / 100
+        value_gbp=pl.col.standing_charge + pl.col.value_kwh * pl.col.charge_per_kwh
     )
 )
 
 # check for gaps - do get one!
 dx = df3.filter(pl.col.value_kwh.is_null())
-assert len(dx) == 0  # noqa: S101
+#assert len(dx) == 0  # noqa: S101
 
 df3.write_parquet(PATH / "OUTPUT data octopus tall.parquet")
 
@@ -71,6 +79,7 @@ df3.write_parquet(PATH / "OUTPUT data octopus tall.parquet")
 df4 = (
     df3.pivot(on="type", index="date")
     .join(ds, on="date", how="left", coalesce=True)
+    .with_columns(value_kwh_electric_generation = pl.when(pl.col.value_kwh_electric_generation.is_null() & ~pl.col.value_kwh_electric_export.is_null()).then(pl.col.value_kwh_electric_export).otherwise(pl.col.value_kwh_electric_generation)) 
     .fill_nan(0)
     .fill_null(0)
     .with_columns(
@@ -78,15 +87,10 @@ df4 = (
         - pl.col.value_kwh_electric_export
     )
     .with_columns(
-        value_gbp_electric_import_avoided=(
-            pl.col.charge_per_kwh_electric_import
-            + pl.col.charge_per_kwh_electric_export
-        )
-        * pl.col.value_kwh_electric_generation_consumed
+        value_gbp_electric_import_avoided=pl.col.charge_per_kwh_electric_import*pl.col.value_kwh_electric_generation_consumed
     )
     .with_columns(
-        value_gbp_electric_savings=pl.col.value_gbp_electric_import_avoided
-        + pl.col.value_gbp_electric_export
+        value_gbp_electric_savings=pl.col.value_gbp_electric_import_avoided + pl.col.value_gbp_electric_export
     )
     .with_columns(
         month=pl.col("date").dt.truncate("1mo"),
@@ -95,8 +99,10 @@ df4 = (
     )
     .sort("date")
     .with_columns(
+        cum_value_gbp_electric_export=pl.col.value_gbp_electric_export.cum_sum(),
+        cum_value_gbp_electric_import_avoided=pl.col.value_gbp_electric_import_avoided.cum_sum(),
         cum_value_gbp_electric_savings=pl.col.value_gbp_electric_savings.cum_sum()
     )
 )
-
+df4.write_csv(PATH / "OUTPUT data octopus and solis wide.csv")
 df4.write_parquet(PATH / "OUTPUT data octopus and solis wide.parquet")
